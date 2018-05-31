fe8b4762efc27c5f9fcb65c4e126cebdb77da9a7
kentatogashi <4276550+kentatogashi@users.noreply.github.com>
2018-06-01 07:29:15 +0900

    Fix typo

--- tfidf_classification/predict.py
+++ tfidf_classification/predict.py
@@ -1,5 +1,6 @@
 # Idea https://towardsdatascience.com/machine-learning-nlp-text-classification-using-scikit-learn-python-and-nltk-c52b92a7c73a
 # Data https://www.kaggle.com/rtatman/deceptive-opinion-spam-corpus 
+# Desc predict polarity and deceptive rate.
 
 import numpy as np
 import pandas as pd
@@ -12,14 +13,14 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('deceptive-opinion.csv')
 # print(df.head())
 X_train, X_test, y_train, y_test = train_test_split(df.text, df.polarity, test_size=0.3)
-text_clf = Pipeline([('vev', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
+text_clf = Pipeline([('vec', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
 text_clf = text_clf.fit(X_train, y_train)
 predicted = text_clf.predict(X_test)
 print('predict polarity correct rate')
 print(np.mean(predicted == y_test))
 
 X_train, X_test, y_train, y_test = train_test_split(df.text, df.deceptive, test_size=0.3)
-text_clf = Pipeline([('vev', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
+text_clf = Pipeline([('vec', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
 text_clf = text_clf.fit(X_train, y_train)
 predicted = text_clf.predict(X_test)
 print('predict deceptive correct rate')
